12ff2e898ad620d2d9e45c9ec7f435c518c45245
Vlad Petriv <vladpetriv05@gmail.com>
2023-04-08 20:46:19 +0200

    feat: add config

---
+++ config/config.go
@@ -0,0 +1,42 @@
+package config
+
+import (
+	"log"
+	"sync"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+// Config represents an app config.
+type Config struct {
+	Telegram Telegram
+	MongoDB  MongoDB
+}
+
+// Telegram represents Telegram bot configuration.
+type Telegram struct {
+	BotToken string `env:"BOT_TOKEN"`
+}
+
+// MongoDB represents MongoDB database configuration.
+type MongoDB struct {
+	URI      string `env:"MONGODB_URI" env-default:"mongodb://localhost:27017"`
+	Database string `env:"MONGODB_DATABASE"`
+}
+
+var (
+	config Config
+	once   sync.Once
+)
+
+// Get returns a Config.
+func Get() *Config {
+	once.Do(func() {
+		err := cleanenv.ReadEnv(&config)
+		if err != nil {
+			log.Fatalf("read env: %v", err)
+		}
+	})
+
+	return &config
+}
