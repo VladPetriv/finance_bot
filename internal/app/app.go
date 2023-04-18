6d829f8f83ff4bfd4431ef7e5572b2b820421ef0
Vlad Petriv <vladpetriv05@gmail.com>
2023-04-18 20:52:15 +0200

    feat: init needed module and services in run app function

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,9 +1,27 @@
 package app
 
 import (
+	"fmt"
+
 	"github.com/VladPetriv/finance_bot/config"
+	"github.com/VladPetriv/finance_bot/internal/service"
+	"github.com/VladPetriv/finance_bot/pkg/bot"
 	"github.com/VladPetriv/finance_bot/pkg/logger"
 )
 
 // Run is used to start the application.
-func Run(cfg *config.Config, logger *logger.Logger) {}
+func Run(cfg *config.Config, logger *logger.Logger) {
+	bot := bot.NewTelegramgBot(cfg.Telegram.BotToken)
+
+	botAPI, err := bot.NewAPI()
+	if err != nil {
+		logger.Fatal().Err(err).Msg("create new bot api")
+	}
+
+	services := service.Services{
+		MessageService: service.NewMessage(botAPI, logger),
+		EventService:   service.NewEvent(botAPI, logger),
+	}
+
+	fmt.Printf("services: %v\n", services)
+}
