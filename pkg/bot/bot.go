0c478cf2f2227679d2a39c7c800c37ae4bcdb687
Vlad Petriv <vladpetriv05@gmail.com>
2023-04-29 22:35:19 +0200

    chore: make send options as pointer

--- pkg/bot/bot.go
+++ pkg/bot/bot.go
@@ -11,9 +11,10 @@ type API interface {
 	// ReadUpdates is used to get all user actions.
 	ReadUpdates(result chan []byte, errors chan error)
 	// Send is used to send information to user.
-	Send(opts SendOptions) error
+	Send(opts *SendOptions) error
 }
 
+// SendOptions represents input structure for Send method.
 type SendOptions struct {
 	ChatID         int64
 	Message        string
