bc083aa40c2096b2fcd0e3408baebce7328b2036
Vlad Petriv <vladpetriv05@gmail.com>
2023-04-30 12:14:33 +0200

    feat: add input structure instead of 2 args

--- internal/service/message.go
+++ internal/service/message.go
@@ -1,6 +1,8 @@
 package service
 
 import (
+	"fmt"
+
 	"github.com/VladPetriv/finance_bot/pkg/bot"
 	"github.com/VladPetriv/finance_bot/pkg/logger"
 )
@@ -20,6 +22,18 @@ func NewMessage(botAPI bot.API, logger *logger.Logger) *messageService {
 	}
 }
 
-func (m messageService) SendMessage(chatID int64, message string) error {
+func (m messageService) SendMessage(opts *SendMessageOptions) error {
+	logger := m.logger
+
+	err := m.botAPI.Send(&bot.SendOptions{
+		ChatID:  opts.ChantID,
+		Message: opts.Text,
+	})
+	if err != nil {
+		logger.Error().Err(err).Msg("send message")
+		return fmt.Errorf("send message: %w", err)
+	}
+
+	logger.Info().Msg("successfully sent message")
 	return nil
 }
