a057f8b7a331c6c1793392119dd347bad121e683
Vlad Petriv <vladpetriv05@gmail.com>
2023-05-01 07:30:37 +0200

    style: change error message

--- internal/service/message.go
+++ internal/service/message.go
@@ -26,12 +26,12 @@ func (m messageService) SendMessage(opts *SendMessageOptions) error {
 	logger := m.logger
 
 	err := m.botAPI.Send(&bot.SendOptions{
-		ChatID:  opts.ChantID,
+		ChatID:  opts.ChatID,
 		Message: opts.Text,
 	})
 	if err != nil {
-		logger.Error().Err(err).Msg("send message")
-		return fmt.Errorf("send message: %w", err)
+		logger.Error().Err(err).Msg("send message via bot api")
+		return fmt.Errorf("send message via bot api: %w", err)
 	}
 
 	logger.Info().Msg("successfully sent message")
