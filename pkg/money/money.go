7073f3ec35691a9a94b1cd360609d23659062371
Vlad Petriv <vladpetriv05@gmail.com>
2023-06-06 20:58:48 +0200

    feat: add money type

---
+++ pkg/money/money.go
@@ -0,0 +1,69 @@
+package money
+
+import (
+	"github.com/shopspring/decimal"
+)
+
+// Money represents custom typo for processing money.
+type Money struct {
+	decimal decimal.Decimal
+}
+
+// Zero represents zero (0) amount.
+// Zero always equals to 0 and to 0.0...N.
+var Zero = NewFromInt(0)
+
+// NewFromString parses string and returns decimal amount.
+// Returns truncated off amount always with precision=2. If s is zero,
+// will be returned Zero decimal without throwing an error.
+func NewFromString(s string) (Money, error) {
+	if len(s) == 0 {
+		return Zero, nil
+	}
+	d, err := decimal.NewFromString(s)
+	if err != nil {
+		return Zero, err
+	}
+	return Money{d}, nil
+}
+
+// NewFromFloat returns decimal from floating-point number.
+// Returns truncated off amount always with precision=2.
+func NewFromFloat(f float64) Money {
+	d := decimal.NewFromFloat(f)
+	return Money{d}
+}
+
+// NewFromInt returns decimal from integer number.
+func NewFromInt(i int64) Money {
+	d := decimal.NewFromInt(i)
+	return Money{d}
+}
+
+// Add returns left + right amounts.
+func (m Money) Add(right Money) Money {
+	return Money{m.decimal.Add(right.decimal)}
+}
+
+// Sub returns left - right amounts.
+func (m Money) Sub(right Money) Money {
+	return Money{m.decimal.Sub(right.decimal)}
+}
+
+// Inc increments left amount by right.
+// Same as left = left + right; left+=right
+func (m *Money) Inc(right Money) {
+	m.decimal = m.decimal.Add(right.decimal)
+}
+
+// Dec decrements left amount by right.
+// Same as left = left - right; left-=right
+func (m *Money) Dec(right Money) {
+	m.decimal = m.decimal.Sub(right.decimal)
+}
+
+// String returns string representation of float with 2 places after digit.
+// Resulting string will be rounded to nearest.
+func (m Money) String() string {
+	return m.decimal.StringFixed(2)
+}
