e077bd2de39f8a2b644325364600e07830dec2ae
Vlad Petriv <vladpetriv05@gmail.com>
2023-08-24 22:05:26 +0200

    feat: add method for converting text to creation period

--- internal/models/operation.go
+++ internal/models/operation.go
@@ -1,6 +1,8 @@
 package models
 
-import "time"
+import (
+	"time"
+)
 
 // Operation represent a financial operation.
 type Operation struct {
@@ -24,6 +26,23 @@ const (
 
 // CreationPeriod defines constants representing different time periods for creation operations.
 type CreationPeriod string
+
+// GetCreationPeriodFromText checks if the input text matches any of the CreationPeriod enums.
+// If there is no match, it returns nil. Otherwise, it returns the corresponding CreationPeriod type.
+func GetCreationPeriodFromText(value string) *CreationPeriod {
+	switch value {
+	case string(CreationPeriodDay):
+		return &CreationPeriodDay
+	case string(CreationPeriodWeek):
+		return &CreationPeriodWeek
+	case string(CreationPeriodMonth):
+		return &CreationPeriodMonth
+	case string(CreationPeriodYear):
+		return &CreationPeriodYear
+	default:
+		return nil
+	}
+}
 
 var (
 	// CreationPeriodDay represents a daily time period.
