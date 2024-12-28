a6679eee9867a4cca427b45c81db430cc9efb1c7
Vlad Petriv <vladpetriv05@gmail.com>
2024-12-28 16:14:15 +0100

    chore: fix linter comments

--- pkg/errs/errs.go
+++ pkg/errs/errs.go
@@ -1,11 +1,13 @@
 package errs
 
+// Err represents a custom error type with a message.
 type Err struct { //nolint:errname
 	Message string `json:"message"`
 }
 
 var _ error = (*Err)(nil)
 
+// New creates a new custom error with the given message.
 func New(message string) *Err {
 	return &Err{Message: message}
 }
@@ -14,6 +16,7 @@ func (e *Err) Error() string {
 	return e.Message
 }
 
+// IsExpected checks if the given error is of custom Err type.
 func IsExpected(err error) bool {
 	_, ok := err.(*Err) //nolint:errorlint
 	return ok
--- pkg/errs/errs_test.go
+++ pkg/errs/errs_test.go
@@ -28,6 +28,7 @@ func Test_IsExpected(t *testing.T) {
 		},
 	}
 	for _, tc := range testCases {
+		tc := tc
 		t.Run(tc.name, func(t *testing.T) {
 			t.Parallel()
 
