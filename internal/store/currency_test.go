11d1031d7b011decb4e03d9648b2f9c823515343
Vlad Petriv <vladpetriv05@gmail.com>
2025-02-26 20:17:35 +0100

    test(store/currency): add test cases for Exists method

--- internal/store/currency_test.go
+++ internal/store/currency_test.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/VladPetriv/finance_bot/config"
 	"github.com/VladPetriv/finance_bot/internal/models"
+	"github.com/VladPetriv/finance_bot/internal/service"
 	"github.com/VladPetriv/finance_bot/internal/store"
 	"github.com/VladPetriv/finance_bot/pkg/database"
 	"github.com/google/uuid"
@@ -266,6 +267,73 @@ func TestCurrency_List(t *testing.T) {
 				assert.Equal(t, tc.preconditions[i].Name, currency.Name)
 				assert.Equal(t, tc.preconditions[i].Symbol, currency.Symbol)
 			}
+		})
+	}
+}
+
+func TestCurrency_Exists(t *testing.T) {
+	t.Parallel()
+
+	ctx := context.Background() //nolint: forbidigo
+	cfg := config.Get()
+
+	db, err := database.NewMongoDB(ctx, cfg.MongoDB.URI, "currency_exists_test")
+	require.NoError(t, err)
+
+	t.Cleanup(func() {
+		err := db.DB.Drop(ctx)
+		assert.NoError(t, err)
+	})
+
+	currencyStore := store.NewCurrency(db)
+
+	currencyID := uuid.NewString()
+
+	testCases := [...]struct {
+		desc          string
+		preconditions *models.Currency
+		args          service.ExistsCurrencyFilter
+		expected      bool
+	}{
+		{
+			desc: "negative: currency by id exists",
+			preconditions: &models.Currency{
+				ID:     currencyID,
+				Name:   "US Dollar",
+				Symbol: "$",
+				Code:   "USD",
+			},
+			expected: true,
+		},
+		{
+			desc: "negative: currency by id doesn't exists",
+			args: service.ExistsCurrencyFilter{
+				ID: uuid.NewString(),
+			},
+			expected: false,
+		},
+	}
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.desc, func(t *testing.T) {
+			t.Parallel()
+
+			if tc.preconditions != nil {
+				err := currencyStore.CreateIfNotExists(ctx, tc.preconditions)
+				assert.NoError(t, err)
+			}
+
+			t.Cleanup(func() {
+				if tc.preconditions != nil {
+					_, err := currencyStore.DB.Collection("Currencies").DeleteOne(ctx, bson.M{"_id": tc.preconditions.ID})
+					assert.NoError(t, err)
+				}
+			})
+
+			actual, err := currencyStore.Exists(ctx, tc.args)
+			assert.NoError(t, err)
+			assert.Equal(t, tc.expected, actual)
+
 		})
 	}
 }
