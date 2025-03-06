eafc1b31a86eb5770776e71d17ce09a655111260
Vlad Petriv <vladpetriv05@gmail.com>
2025-03-06 21:40:03 +0100

    feat(models/currency): replace tags bson->db

--- internal/models/currency.go
+++ internal/models/currency.go
@@ -2,8 +2,8 @@ package models
 
 // Currency represents currency model which contains currency name, code and symbol
 type Currency struct {
-	ID     string `bson:"_id"`
-	Name   string `bson:"name"`
-	Code   string `bson:"code"`
-	Symbol string `bson:"symbol"`
+	ID     string `db:"id"`
+	Name   string `db:"name"`
+	Code   string `db:"code"`
+	Symbol string `db:"symbol"`
 }
