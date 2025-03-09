0f49eadc489f8a37c0d30209e0f3610b20b28c6b
Vlad Petriv <vladpetriv05@gmail.com>
2025-03-09 17:43:09 +0100

    feat(models/state): add custom sql driver method implementations for steps and metadata fields

--- internal/models/state.go
+++ internal/models/state.go
@@ -1,6 +1,9 @@
 package models
 
 import (
+	"database/sql/driver"
+	"encoding/json"
+	"fmt"
 	"slices"
 	"strings"
 	"time"
@@ -12,12 +15,12 @@ import (
 // State represents the current state of a user's interaction with the bot
 type State struct {
 	ID     string `db:"id"`
-	UserID string `db:"user_id"`
+	UserID string `db:"user_username"`
 
-	Flow  Flow       `db:"flow"`
-	Steps []FlowStep `db:"steps"`
+	Flow  Flow      `db:"flow"`
+	Steps FlowSteps `db:"steps"`
 
-	Metedata map[string]any `db:"metadata"`
+	Metedata Metadata `db:"metadata"`
 
 	CreatedAt time.Time `db:"created_at"`
 	UpdatedAt time.Time `db:"updated_at"`
@@ -197,6 +200,57 @@ const (
 	// UpdateOperationFlow represents the flow for updating an operation
 	UpdateOperationFlow Flow = "update_operation"
 )
+
+// FlowSteps represents a slice of FlowStep
+type FlowSteps []FlowStep
+
+// Value implements the driver.Valuer interface
+func (s FlowSteps) Value() (driver.Value, error) {
+	if s == nil {
+		return nil, nil
+	}
+	return json.Marshal(s)
+}
+
+// Scan implements the sql.Scanner interface
+func (s *FlowSteps) Scan(value interface{}) error {
+	if value == nil {
+		*s = nil
+		return nil
+	}
+
+	bytes, ok := value.([]byte)
+	if !ok {
+		return fmt.Errorf("expected []byte, got %T", value)
+	}
+
+	return json.Unmarshal(bytes, s)
+}
+
+type Metadata map[string]any
+
+// Value implements the driver.Valuer interface
+func (s Metadata) Value() (driver.Value, error) {
+	if s == nil {
+		return nil, nil
+	}
+	return json.Marshal(s)
+}
+
+// Scan implements the sql.Scanner interface
+func (s *Metadata) Scan(value interface{}) error {
+	if value == nil {
+		*s = nil
+		return nil
+	}
+
+	bytes, ok := value.([]byte)
+	if !ok {
+		return fmt.Errorf("expected []byte, got %T", value)
+	}
+
+	return json.Unmarshal(bytes, s)
+}
 
 // FlowStep represents a specific step within a flow
 type FlowStep string
