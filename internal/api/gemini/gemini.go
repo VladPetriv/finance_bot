2524e57abbd13e3282d2b6b74a13acb68aaa5f79
Vlad Petriv <vladpetriv05@gmail.com>
2025-03-15 22:44:57 +0100

    feat: implement gemini integration

---
+++ internal/api/gemini/gemini.go
@@ -0,0 +1,69 @@
+package gemini
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	"github.com/google/generative-ai-go/genai"
+	"google.golang.org/api/option"
+)
+
+type gemini struct {
+	client *genai.Client
+
+	model string
+}
+
+func New(ctx context.Context, apiKey, model string) (*gemini, error) {
+	client, err := genai.NewClient(ctx, option.WithAPIKey(apiKey))
+	if err != nil {
+		return nil, fmt.Errorf("init gemini client: %w", err)
+	}
+
+	return &gemini{
+		client: client,
+		model:  model,
+	}, nil
+}
+
+var emptyResponse = errors.New("empty response")
+
+func (g *gemini) Execute(ctx context.Context, prompt string) (string, error) {
+	response, err := g.client.GenerativeModel(g.model).GenerateContent(ctx, genai.Text(prompt))
+	if err != nil {
+		return "", fmt.Errorf("generate content through model: %w", err)
+	}
+	if len(response.Candidates) == 0 {
+		return "", emptyResponse
+	}
+
+	var output string
+
+candidateLoop:
+	for _, candidate := range response.Candidates {
+		if candidate.Content != nil {
+			for _, part := range candidate.Content.Parts {
+
+				data, ok := part.(genai.Text)
+				if !ok {
+					continue
+				}
+
+				output += string(data)
+			}
+
+			break candidateLoop
+		}
+	}
+
+	return output, nil
+}
+
+func (g *gemini) Close() error {
+	if g.client != nil {
+		return g.client.Close()
+	}
+
+	return nil
+}
